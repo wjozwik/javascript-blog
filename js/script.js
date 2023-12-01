5b78b7435c061a3c1f1c86305218b1724244ba95
Wojciech Jozwik <wj.telefon@gmail.com>
2023-12-01 14:10:37 +0100

    Update links event listener (add 'active' class to selected link)

--- js/script.js
+++ js/script.js
@@ -8,17 +8,19 @@
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
+    const clickedElement = this;
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks) {
       activeLink.classList.remove('active');
     }
-    
-    /* add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
     
     for(let activeArticle of activeArticles) {
