c7dbca440b0005581dc8e0b440ab42bd4f83ec6b
Wojciech Jozwik <wj.telefon@gmail.com>
2023-12-01 13:42:32 +0100

    Update links event listener (remove 'active' class)

--- js/script.js
+++ js/script.js
@@ -4,21 +4,31 @@
 //   const links = document.querySelectorAll('.titles a');
 //   console.log('links:', links);
 // });
-
-const titleClickHandler = function(event){
+{
+  const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
-  
+
     /* remove class 'active' from all article links  */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }
+    
     /* add class 'active' to the clicked link */
-  
+
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for(let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
     /* add class 'active' to the correct article */
   }
   
@@ -27,3 +37,4 @@ const titleClickHandler = function(event){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
