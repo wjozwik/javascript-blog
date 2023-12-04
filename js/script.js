e3c6de6df1d88f51d571023dae89d207b47c91c9
Wojciech Jozwik <wj.telefon@gmail.com>
2023-12-04 21:36:19 +0100

    Add links list generator to js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// document.getElementById('test-button').addEventListener('click', function(){
-//   const links = document.querySelectorAll('.titles a');
-//   console.log('links:', links);
-// });
 {
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -34,11 +34,42 @@
 
     /* [DONE] add class 'active' to the correct article */
     selectedArticle.classList.add('active');
-  }
+  };
   
-  const links = document.querySelectorAll('.titles a');
+  const generateTitleLinks = function() {
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles) {
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* find the title element */
+      /* get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+      /* insert link into titleList */
+      html = html + linkHTML;
+    }
+
+    titleList.innerHTML = html;
+    
+    const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+  };
+
+  generateTitleLinks();
 }
