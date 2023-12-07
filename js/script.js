4a429be2a42e128454a1ed28d1bfde812b893804
Wojciech Jozwik <wj.telefon@gmail.com>
2023-12-07 20:32:31 +0100

    Add tags generator in js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 {
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
 
   const titleClickHandler = function(event){
     event.preventDefault();
@@ -72,4 +73,46 @@
   };
 
   generateTitleLinks();
+
+  const generateTags = function() {
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    /* START LOOP: for every article: */
+    for(let article of articles) {
+
+      /* find tags wrapper */
+      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+      tagsWrapper.innerHTML = '';
+
+      /* make html variable with empty string */
+      let html = '';
+
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+
+      /* split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+
+      /* START LOOP: for each tag */
+      for(let tag of articleTagsArray){
+
+        /* generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+
+        /* add generated code to html variable */
+        html = html + linkHTML;
+
+        /* END LOOP: for each tag */
+      }
+      /* insert HTML of all the links into the tags wrapper */
+      tagsWrapper.innerHTML = html;
+
+      /* END LOOP: for every article: */
+    }
+  };
+
+  generateTags();
 }
+
+
